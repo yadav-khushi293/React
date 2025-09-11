eaf925faaefedea002744656560ee639a8666acb
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-09-11 23:04:01 +0530

    xyz

--- Profiler/src/main.jsx
+++ Profiler/src/main.jsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import {Profile} from './App.jsx'
-
+import{Profiles} from '../Componet/Profile.jsx'
 createRoot(document.getElementById('root')).render(
   <>
     <Profile />
+    
   </>,
 )
