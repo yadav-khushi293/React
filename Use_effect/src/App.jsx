ff144d8f9f92df3dfeaab6077bd160c9c0fe6413
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-09-20 16:29:19 +0530

    useeffect

--- Use_effect/src/App.jsx
+++ Use_effect/src/App.jsx
@@ -1,36 +1,64 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './App.css'
 
-export const App=()=>{
+// export const App=()=>{
 
-  const [count,setCount] =React.useState(0);
-  const [count2,setCount2] = React.useState(10);
+//   const [count,setCount] =React.useState(0);
+//   const [count2,setCount2] = React.useState(10);
 
-  React.useEffect(()=>{
-    console.log("Count:always run",count);
-  });
+//   React.useEffect(()=>{
+//     console.log("Count:always run",count);
+//   });
 
-  React.useEffect(()=>{
-    console.log("count:have empty run",count);
-  } ,[]);
+//   React.useEffect(()=>{
+//     console.log("count:have empty run",count);
+//   } ,[]);
 
-  React.useEffect(()=>{
-    console.log("count:have same dependency",count2);
-  },[count2]);
+//   React.useEffect(()=>{
+//     console.log("count:have same dependency",count2);
+//   },[count2]);
 
+
+//   return(
+//     <>
+
+//     <h1>Count_1{count}</h1>
+//     <h1>Count_2 {count2}</h1>
+
+//     <button onClick={()=>setCount((prev)=>prev-1)}>-</button>
+//       <button onClick={()=>setCount((prev)=>prev+1)}>+</button>
+//       <button onClick={()=>setCount2((prev)=>prev-1)}>count2 decrement</button>
+//           <button onClick={()=>setCount2((prev)=>prev+1)}>count2 increment</button>
+//     </>
+//   )
+// }
+
+
+export const AddTodo =()=> {
+
+
+  const data = useRef(null);
+
+  const handleTodo =()=>{
+
+       if(data.current.value.trim()==="")return
+       let todo ={
+      id:Date.now(),
+      todo:data.current.value,
+      isEdits:false,
+      isComplete:false
+    };
+  
+    sessionStorage.setItem("todos",JSON.stringify(todo));
+
+  }
 
   return(
     <>
+    <h1>todo</h1>
+    <input type="text" placeholder='enter the todo....' ref={data}></input>
+    <button onClick={handleTodo}>Add</button>
 
-    <h1>Count_1{count}</h1>
-    <h1>Count_2 {count2}</h1>
-
-    <button onClick={()=>setCount((prev)=>prev-1)}>-</button>
-      <button onClick={()=>setCount((prev)=>prev+1)}>+</button>
-      <button onClick={()=>setCount2((prev)=>prev-1)}>count2 decrement</button>
-          <button onClick={()=>setCount2((prev)=>prev+1)}>count2 increment</button>
     </>
   )
-}
-
-
+  }
--- Use_effect/src/main.jsx
+++ Use_effect/src/main.jsx
@@ -1,9 +1,11 @@
 
 import { createRoot } from 'react-dom/client'
-import {App} from './App.jsx'
+// import {App} from './App.jsx'
+import {AddTodo} from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    {/* <App /> */}
+    <AddTodo/>
   </>,
 )
