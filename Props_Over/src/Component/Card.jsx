ab65d21d7bce93dcacef9534fcb4863e5b0d6ea4
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-10-16 19:19:21 +0530

    xyz

--- Props_Over/src/Component/Card.jsx
+++ Props_Over/src/Component/Card.jsx
@@ -13,8 +13,7 @@ export const Card = ({
         <h1>name:{name}</h1>
         <h1>age:{age}</h1>
         <h1>email:{email}</h1>
-        <h2>email:</h2>
-        <h2>gmail</h2>
+        
         </>
     )
 }
