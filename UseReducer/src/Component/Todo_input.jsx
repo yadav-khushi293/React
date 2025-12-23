f24819162fd14ef18d60894b730991bbfccee521
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-12-23 16:35:08 +0530

    useReducer

--- UseReducer/src/Component/Todo_input.jsx
+++ UseReducer/src/Component/Todo_input.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { reducer } from '../ReducerHook/Reducer';
 import { initialValue } from '../ReducerHook/Store';
-import { ADD_TODO_ITEMS } from '../ReducerHook/Action';
+import { ADD_TODO_ITEMS, ERROR_TODO_ITEMS } from '../ReducerHook/Action';
 import { Todo_list } from './Todo-list';
 
 export const TodoInput = () => {
@@ -12,18 +12,27 @@ export const TodoInput = () => {
 
   const handleAddTodo = () => {
     const value = todoText.current.value;
+    if(value===""){
+      dispatch({type:ERROR_TODO_ITEMS});
+      return;
+    }
     dispatch({ type: ADD_TODO_ITEMS, payload: value });
+
+    setTimeout(()=>{
+      dispatch({type:ADD_TODO_ITEMS,payload:value})
+    },1000)
+    todoText.current.value='';
   };
 
   return (
     <>
-      <input
-        ref={todoText}
-        type="text"
-        placeholder="enter your task!!!"
-      />
+      <input ref={todoText} type="text" placeholder="enter your task!!!"/>
       <button onClick={handleAddTodo}>add task</button>
-      <Todo_list  value={{ state, dispatch }} />
+
+      {state.isError?(<h1>Somthing went wrong...</h1>):(
+        <Todo_list  value={{ state, dispatch }} />
+      )}
+      
     </>
   );
 };
--- UseReducer/src/ReducerHook/Reducer.js
+++ UseReducer/src/ReducerHook/Reducer.js
@@ -1,13 +1,23 @@
 import { data } from 'autoprefixer';
-import {DELETES_TODO_ITEMS,ADD_TODO_ITEMS,EDITS_TODO_ITEMS} from './Action';;
+import {DELETES_TODO_ITEMS,ADD_TODO_ITEMS,EDITS_TODO_ITEMS,LOADIN_TODO_ITEMS,ERROR_TODO_ITEMS} from './Action';;
 import {initialValue} from './Store';
 
 export const reducer =(state = initialValue , action)=>{
 
     switch(action.type){
+
+       case LOADIN_TODO_ITEMS:
+       return{...state,isLoading:true};
+
+       
+       case ERROR_TODO_ITEMS:
+       return{...state,isLoading:false,isError:true}
+
+
         case ADD_TODO_ITEMS:
             return{
-                ...state,
+                ...state,isLoading:false,
+                isError:false,
                 items:[
                     ...state.items,
                     {
@@ -19,6 +29,7 @@ export const reducer =(state = initialValue , action)=>{
                 ],
             };
            
+
             case EDITS_TODO_ITEMS:
                 if(action.payload.updateText && action.payload.id){
                     return{
@@ -39,6 +50,7 @@ export const reducer =(state = initialValue , action)=>{
                             ),
                         }
                     };
+
                 
             case DELETES_TODO_ITEMS:
                 return{
